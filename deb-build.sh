c2ba2710418a4aa3e0def6ad4656046e06afed5a
Giovanni <giovannirafanan609@gmail.com>
2025-12-21 16:06:42 +0800

    simplified script process

--- deb-build.sh
+++ deb-build.sh
@@ -3,6 +3,5 @@ set -e
 
 distrobox enter ubuntu -- bash -c '
   cp ~/Projects/practice-gnome-app/build/src/memerist ~/Projects/ubuntubox/memerist_0.1.0-1_amd64/usr/bin/
-  cd ~/Projects/ubuntubox
-  dpkg-deb --build memerist_0.1.0-1_amd64
+  dpkg-deb --build ~/Projects/ubuntubox/memerist_0.1.0-1_amd64
 '
